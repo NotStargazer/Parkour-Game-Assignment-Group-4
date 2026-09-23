cf545475b77bac2253e2d416354b6c8632921ae8
SlaveKnightGael141 <ozankesen10@gmail.com>
2026-09-23 12:48:41 +0200

    Refactor TokenPickUp to TokenPickup with scoring
    Updated TokenPickUp class to TokenPickup and added scoreValue functionality.

--- TokenPickUp.cs
+++ TokenPickUp.cs
@@ -1,18 +1,27 @@
 using UnityEngine;
 
-public class TokenPickUp : MonoBehaviour
+public class TokenPickup : MonoBehaviour
 {
+    // Detta gör att ni kan ändra poängvärdet (5 eller 20) direkt i Unitys Inspector
+    public int scoreValue = 5; 
+
     private void OnTriggerEnter(Collider other)
-
     {
-
+        // 1. Kolla om det är spelaren som krockar
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Token Collected");
+            // 2. Leta efter poängväskan på spelaren
+            PlayerInventory playerInv = other.GetComponent<PlayerInventory>();
 
+            // 3. Om väskan finns, skicka över poängen
+            if (playerInv != null)
+            {
+                playerInv.AddPoints(scoreValue);
+            }
 
+            // 4. Logga i konsolen och ta bort token från banan
+            Debug.Log("Token Collected!");
             Destroy(gameObject);
-        
         }
     }
 }
