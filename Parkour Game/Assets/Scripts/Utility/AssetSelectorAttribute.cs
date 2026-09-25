24a1971948bff8e24a2f09eba3bd8d0de7001d0a
Stargazer <54099294+NotStargazer@users.noreply.github.com>
2026-09-25 11:55:40 +0200

    Fun level objects (#11)

---
+++ Parkour Game/Assets/Scripts/Utility/AssetSelectorAttribute.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Utility
+{
+    public class AssetSelectorAttribute : PropertyAttribute
+    {
+        public readonly string Path;
+        public readonly string AssetType;
+        public AssetSelectorAttribute(string path, string assetType)
+        {
+            Path = path;
+            AssetType = assetType;
+        }
+
+    }
+}
---
+++ Parkour Game/Assets/Scripts/Utility/MeshBuilder.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utility
+{
+    public ref struct MeshBuilder
+    {
+        private List<Vector3> _verts;
+        private List<Vector3> _normals;
+        private List<Vector2> _uvs;
+        private List<int> _triangles;
+            
+        private void AddCubePlane(Vector3 normal, Vector2 uvScale, params Vector3[] verts)
+        {
+            uvScale *= 0.5f;
+            foreach (var vert in verts)
+            {
+                _verts.Add(vert);
+                _normals.Add(normal);
+            }
+            
+            _uvs.Add(new Vector2(-uvScale.x, -uvScale.y));
+            _uvs.Add(new Vector2(-uvScale.x,  uvScale.y));
+            _uvs.Add(new Vector2( uvScale.x,  uvScale.y));
+            _uvs.Add(new Vector2( uvScale.x, -uvScale.y));
+
+            var lastTri = _triangles.Count > 0 ? _triangles[^1] + 1 : 0;
+            _triangles.Add(lastTri);
+            _triangles.Add(lastTri + 1);
+            _triangles.Add(lastTri + 2);
+            _triangles.Add(lastTri);
+            _triangles.Add(lastTri + 2);
+            _triangles.Add(lastTri + 3);
+        }
+
+        public static Mesh CreateUVScaledCubeMesh(Vector3 scale, string name)
+        {
+            var half = Vector3.one * 0.5f;
+            scale *= 0.5f;
+            
+            //Lower/Upper (l/u) Back/Forward (b/f) Left/Right (l/r) lbr - ufr
+            var lbl = new Vector3(-half.x, -half.y, -half.z);
+            var lbr = new Vector3( half.x, -half.y, -half.z);
+            var lfl = new Vector3(-half.x, -half.y,  half.z);
+            var lfr = new Vector3( half.x, -half.y,  half.z);
+            var ubl = new Vector3(-half.x,  half.y, -half.z);
+            var ubr = new Vector3( half.x,  half.y, -half.z);
+            var ufl = new Vector3(-half.x,  half.y,  half.z);
+            var ufr = new Vector3( half.x,  half.y,  half.z);
+
+            var verts = new List<Vector3>();
+            var normals = new List<Vector3>();
+            var uvs = new List<Vector2>();
+            var triangles = new List<int>();
+
+            var meshBuilder = new MeshBuilder
+            {
+                _verts = verts,
+                _normals = normals,
+                _uvs = uvs,
+                _triangles = triangles,
+            };
+
+            meshBuilder.AddCubePlane(Vector3.back,    new Vector2(scale.x, scale.y), lbl, ubl, ubr, lbr);
+            meshBuilder.AddCubePlane(Vector3.forward, new Vector2(scale.x, scale.y), lfr, ufr, ufl, lfl);
+            meshBuilder.AddCubePlane(Vector3.left,    new Vector2(scale.z, scale.y), lfl, ufl, ubl, lbl);
+            meshBuilder.AddCubePlane(Vector3.right,   new Vector2(scale.z, scale.y), lbr, ubr, ufr, lfr);
+            meshBuilder.AddCubePlane(Vector3.down,    new Vector2(scale.x, scale.z), lfl, lbl, lbr, lfr);
+            meshBuilder.AddCubePlane(Vector3.up,      new Vector2(scale.x, scale.z), ubl, ufl, ufr, ubr);
+
+            var generatedMesh = new Mesh
+            {
+                name = name,
+                vertices = meshBuilder._verts.ToArray(),
+                normals = meshBuilder._normals.ToArray(),
+                uv = meshBuilder._uvs.ToArray(),
+                triangles = meshBuilder._triangles.ToArray()
+            };
+            
+            generatedMesh.RecalculateBounds();
+            generatedMesh.RecalculateTangents();
+
+            return generatedMesh;
+        }
+
+        public static Mesh CreateUVScaledSlopeMesh(Vector3 scale, float thickness, string name)
+        {
+            var half = Vector3.one * 0.5f;
+            var offset = thickness / scale.y;
+            scale *= 0.5f;
+            thickness *= 0.5f;
+            
+            //Lower/Upper (l/u) Back/Forward (b/f) Left/Right (l/r) lbr - ufr
+            var lbl = new Vector3(-half.x, -half.y, -half.z);
+            var lfl = new Vector3(-half.x, -half.y,  half.z);
+            var ubr = new Vector3( half.x,  half.y, -half.z);
+            var ufr = new Vector3( half.x,  half.y,  half.z);
+            
+            var ubl = new Vector3(-half.x, -half.y + offset, -half.z);
+            var ufl = new Vector3(-half.x, -half.y + offset,  half.z);
+            var lbr = new Vector3( half.x,  half.y - offset, -half.z);
+            var lfr = new Vector3( half.x,  half.y - offset,  half.z);
+            
+            var verts = new List<Vector3>();
+            var normals = new List<Vector3>();
+            var uvs = new List<Vector2>();
+            var triangles = new List<int>();
+
+            var meshBuilder = new MeshBuilder
+            {
+                _verts = verts,
+                _normals = normals,
+                _uvs = uvs,
+                _triangles = triangles,
+            };
+
+            Vector3 lhs;
+            lhs.x = ubr.x - ubl.x;
+            lhs.y = ubr.y - ubl.y;
+            lhs.z = ubr.z - ubl.z;
+            Vector3 rhs;
+            rhs.x = ufl.x - ubl.x;
+            rhs.y = ufl.y - ubl.y;
+            rhs.z = ufl.z - ubl.z;
+            var slopeNormal = Vector3.Cross(in lhs, in rhs).normalized;
+            var slopeHypotenuse = Vector3.Distance(ubl, ubr);
+            
+            meshBuilder.AddCubePlane(Vector3.back,    new Vector2(scale.x, thickness),       lbl, ubl, ubr, lbr);
+            meshBuilder.AddCubePlane(Vector3.forward, new Vector2(scale.x, thickness),       lfr, ufr, ufl, lfl);
+            meshBuilder.AddCubePlane(Vector3.left,    new Vector2(scale.z, thickness),       lfl, ufl, ubl, lbl);
+            meshBuilder.AddCubePlane(Vector3.right,   new Vector2(scale.z, thickness),       lbr, ubr, ufr, lfr);
+            meshBuilder.AddCubePlane(slopeNormal,     new Vector2(slopeHypotenuse, scale.z), lfl, lbl, lbr, lfr);
+            meshBuilder.AddCubePlane(-slopeNormal,    new Vector2(slopeHypotenuse, scale.z), ubl, ufl, ufr, ubr);
+            
+            var generatedMesh = new Mesh
+            {
+                name = name,
+                vertices = meshBuilder._verts.ToArray(),
+                normals = meshBuilder._normals.ToArray(),
+                uv = meshBuilder._uvs.ToArray(),
+                triangles = meshBuilder._triangles.ToArray()
+            };
+            
+            generatedMesh.RecalculateBounds();
+            generatedMesh.RecalculateTangents();
+
+            return generatedMesh;
+        }
+    }
+}
